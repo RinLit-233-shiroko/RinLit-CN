c9286f15f804e0f7ec65a74a04f45d58bdff49d0
RinLit <lintusup@foxmail.com>
2026-06-20 17:11:05 +0800

    feat(i18n): 国际化

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,12 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [tailwind()],
+
+  i18n: {
+    locales: ['en', 'zh-cn'],
+    defaultLocale: 'zh-cn',
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
 });
